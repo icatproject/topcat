dcd753af03dc49e43080d2fb4b9a5f91a98f33c4
Laurent Lerusse <l.lerusse@gmail.com>
2009-08-04 16:31:58 +0000

    Created new tabs horizontal and vertical with some description in them. Check the Browse tab.

--- src/uk/topcat/web/client/TopCatWidget.java
+++ src/uk/topcat/web/client/TopCatWidget.java
@@ -27,6 +27,8 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
@@ -75,7 +77,9 @@ public class TopCatWidget extends Composite implements TopCatPresenter.View {
 		tabPanel.setWidth((PAGE_WIDTH - VERTICAL_TAB_LABEL_MAX_WIDTH) + "px");
 		tabPanel.setAnimationEnabled(false);
 
+		
 		//add a keyword tab
+		//-----------------
 		HTML keywordInfo = new HTML(lang.keywordTabLabel());
 		HTML keywordText = new HTML(lang.keywordSearchInstructions());
 
@@ -84,8 +88,9 @@ public class TopCatWidget extends Composite implements TopCatPresenter.View {
 		autoForm = new AutoSuggestForm();
 		autoPanel.add(autoForm);
 		autoPanel.add(new HTML("&nbsp;"));
-		search = new Button(lang.searchButton());
+		search = new Button(lang.cbtn_search());
 		autoPanel.add(search);
+		
 		VerticalPanel keywordPanel = new VerticalPanel();
 		keywordPanel.add(new HTML("&nbsp;"));
 		keywordPanel.add(keywordText);
@@ -99,31 +104,143 @@ public class TopCatWidget extends Composite implements TopCatPresenter.View {
 			}
 		});
 		tabPanel.add(keywordPanel, keywordInfo);
-
-		// Add a browse tab 
-		VerticalPanel vPanel = new VerticalPanel();
-		Image browseImg = new Image("img/TagCloud.jpg");
-		// Image("http://www.wordle.net/thumb/wrdl/1010547/neutrons");
-		vPanel.add(browseImg);
-		HTML browseInfo = new HTML(lang.browseTabLabel());
-		browseInfo.addMouseOverHandler(new MouseOverHandler() {
+		
+		// Add an Advanced tab
+		// -------------------
+		HTML advancedInfo = new HTML(lang.htab_advancedLabel());
+		HTML advancedText = new HTML(lang.htab_advancedDescription());
+		advancedInfo.addMouseOverHandler(new MouseOverHandler() {
 			public void onMouseOver(MouseOverEvent event) {
 				tabPanel.selectTab(1);
 			}
 		});
-		tabPanel.add(vPanel, browseInfo);
+		tabPanel.add(advancedText, advancedInfo);
+		
+		// Add a tag tab 
+		//-----------------
+		HTML tagInfo = new HTML(lang.htab_tagLabel());
+		VerticalPanel vPanel = new VerticalPanel();
+		
+		HTML tagText = new HTML(lang.htab_tagDescription());
+		vPanel.add(tagText);
 
-		// Add an Avanced tab
-		HTML advancedInfo = new HTML(lang.advancedTabLabel());
-		HTML advancedText = new HTML("Advanced Search fields go here");
-		advancedInfo.addMouseOverHandler(new MouseOverHandler() {
+		Image browseImg = new Image("img/TagCloud.jpg");
+		// Image("http://www.wordle.net/thumb/wrdl/1010547/neutrons");
+		vPanel.add(browseImg);
+		
+		tagInfo.addMouseOverHandler(new MouseOverHandler() {
 			public void onMouseOver(MouseOverEvent event) {
 				tabPanel.selectTab(2);
 			}
 		});
-		tabPanel.add(advancedText, advancedInfo);
+		tabPanel.add(vPanel, tagInfo);
 
+		// Add a browse tab 
+		//-----------------
+		HTML browseInfo = new HTML(lang.htab_browseLabel());
+		
+		HorizontalPanel browseHPanel = new HorizontalPanel();
+		
+		
+		VerticalPanel browseVPanel = new VerticalPanel();
+		HTML browseText = new HTML(lang.htab_browseDescription());
+		browseVPanel.add(browseText);
+
+		Tree browseTree = new Tree();
+		
+		TreeItem root = new TreeItem("/");
+		browseTree.addItem(root);
+		
+		TreeItem facility_ISIS = new TreeItem("ISIS/");
+		facility_ISIS.addItem("Cycle 2008-06/");
+		facility_ISIS.addItem("Cycle 2009-01/");
+		facility_ISIS.addItem("Cycle 2009-01/");
+		root.addItem(facility_ISIS);
+		
+		TreeItem facility_DLS = new TreeItem("DLS/");
+		facility_DLS.addItem("2007/");
+		facility_DLS.addItem("2008/");
+		facility_DLS.addItem("2009/");
+		root.addItem(facility_DLS);
+		
+		TreeItem facility_ILL = new TreeItem("ILL/");	
+		facility_ILL.addItem("2002/");
+		facility_ILL.addItem("2003/");
+		facility_ILL.addItem("2004/");
+		
+		TreeItem ill_2005 = new TreeItem("2005/");	
+		ill_2005.addItem("D1A/");
+		ill_2005.addItem("D11/");
+		ill_2005.addItem("IN4/");
+
+		TreeItem ill_instr_IN3 = new TreeItem("IN3/");	
+		ill_2005.addItem(ill_instr_IN3);
+		ill_2005.addItem("IN20/");
+		ill_2005.addItem("Cryo-EDM/");
+		facility_ILL.addItem(ill_2005);
+		facility_ILL.addItem("2006/");
+		facility_ILL.addItem("2007/");
+		facility_ILL.addItem("2008/");
+		facility_ILL.addItem("2009/");		
+		root.addItem(facility_ILL);
+		browseTree.addItem(root);
+		
+		browseVPanel.add(browseTree);
+		browseHPanel.add(browseVPanel);
+		
+		VerticalPanel buttonPanel = new VerticalPanel();
+	
+		Button addToSel = new Button("Add to Selection");
+		Button removeFromSel = new Button("Remove from Selection");
+		buttonPanel.add(addToSel);
+		buttonPanel.add(new HTML("&nbsp;"));
+		buttonPanel.add(removeFromSel);
+		browseHPanel.add(buttonPanel);
+
+		
+		browseInfo.addMouseOverHandler(new MouseOverHandler() {
+			public void onMouseOver(MouseOverEvent event) {
+				tabPanel.selectTab(3);
+			}
+		});
+		tabPanel.add(browseHPanel, browseInfo);
+
+		// Add an My Data tab
+		// -------------------
+		HTML myDataInfo = new HTML(lang.htab_myDataLabel());
+		HTML myDataText = new HTML(lang.htab_myDataDescription());
+		myDataInfo.addMouseOverHandler(new MouseOverHandler() {
+			public void onMouseOver(MouseOverEvent event) {
+				tabPanel.selectTab(4);
+			}
+		});
+		tabPanel.add(myDataText, myDataInfo);
+		
+		// Add an Bookmarks tab
+		// -------------------
+		HTML bookmarksInfo = new HTML(lang.htab_bookmarksLabel());
+		HTML bookmarksText = new HTML(lang.htab_bookmarksDescription());
+		bookmarksInfo.addMouseOverHandler(new MouseOverHandler() {
+			public void onMouseOver(MouseOverEvent event) {
+				tabPanel.selectTab(5);
+			}
+		});
+		tabPanel.add(bookmarksText, bookmarksInfo);
+		
+		
+		// Add an Preferences tab
+		// -----------------------
+		HTML prefInfo = new HTML(lang.htab_preferenceLabel());
+		HTML prefText = new HTML(lang.htab_preferenceDescription());
+		prefInfo.addMouseOverHandler(new MouseOverHandler() {
+			public void onMouseOver(MouseOverEvent event) {
+				tabPanel.selectTab(6);
+			}
+		});
+		tabPanel.add(prefText, prefInfo);
+		
 		// Return the content
+		// ------------------
 		tabPanel.selectTab(0);
 		tabPanel.ensureDebugId("cwTabPanel");
 
@@ -139,14 +256,50 @@ public class TopCatWidget extends Composite implements TopCatPresenter.View {
 		pbmg = new PagingBeanModelGrid();
 		
 		//Create Tabs (including PagingBeanModelGrid on first tab)
-		vtp
-				.add(pbmg,
-						new HTML(lang.searchResultsTabLabel()) 
-						
-						);
-										//new PagingGrid().toolBar);
-		vtp.add(new HTML("...<br><br><br><br><br>"), new HTML(lang.myDataTabLabel()));
-		vtp.add(new HTML("<br><br><br><br><br>"), new HTML("Tagged&nbsp;Data"));
+		
+		// Create Vertical Panels
+		// ----------------------
+		
+		// Welcome
+		VerticalPanel welcomePanel = new VerticalPanel();
+		HTML welcomeText = new HTML(lang.vtab_welcomeDescription());
+		welcomePanel.add(welcomeText);
+		Image pandataLogo = new Image("img/Logos_Pandata.jpg");
+		welcomePanel.add(pandataLogo);
+		vtp.add(welcomePanel, new HTML(lang.vtab_welcomeLabel()));
+		
+		// Investigation
+		VerticalPanel investigationPanel = new VerticalPanel();
+		HTML investigationText = new HTML(lang.vtab_investigationDescription());
+		investigationPanel.add(investigationText);
+		investigationPanel.add(pbmg);
+		vtp.add(investigationPanel,new HTML(lang.vtab_investigationLabel())); 
+
+		// Sample
+		VerticalPanel samplePanel = new VerticalPanel();
+		HTML sampleText = new HTML(lang.vtab_sampleDescription());
+		samplePanel.add(sampleText);
+		samplePanel.add(new PagingBeanModelGrid());	
+		vtp.add(samplePanel,new HTML(lang.vtab_sampleLabel()) );  
+	
+		// Dataset
+		VerticalPanel datasetPanel = new VerticalPanel();
+		HTML datasetText = new HTML(lang.vtab_datasetDescription());
+		datasetPanel.add(datasetText);
+		datasetPanel.add(new PagingBeanModelGrid());	
+		vtp.add(datasetPanel,new HTML(lang.vtab_datasetLabel()) );  
+	
+		// DataFiles
+		VerticalPanel datafilePanel = new VerticalPanel();
+		HTML datafileText = new HTML(lang.vtab_datafileDescription());
+		datafilePanel.add(datafileText);
+		datafilePanel.add(new PagingBeanModelGrid());		
+		vtp.add(datafilePanel,new HTML(lang.vtab_datafileLabel()) );  
+		
+		// Download
+		vtp.add(new HTML(lang.vtab_downloadDescription()), new HTML(lang.vtab_downloadLabel()));
+		vtp.add(new HTML(lang.vtab_permissionDescription()), new HTML(lang.vtab_permissionLabel()));
+		vtp.add(new HTML(lang.vtab_applicationDescription()), new HTML(lang.vtab_applicationLabel()));
 		vtp.selectTab(0);
 		
 		
