ff17134a36be1aca68ba58ed962254be8e09330d
Antony Wilson <antony.wilson@stfc.ac.uk>
2011-06-01 07:57:51 +0000

    up values

--- Code/TopCAT/src/main/java/uk/ac/stfc/topcat/gwt/client/Constants.java
+++ Code/TopCAT/src/main/java/uk/ac/stfc/topcat/gwt/client/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
     //TODO move constants to a config file
     
     /** The MAX_DOWNLOAD_FRAMES relates to the number of __download frames in TOPCATWeb.jsp */
-    public static final int MAX_DOWNLOAD_FRAMES = 4;
+    public static final int MAX_DOWNLOAD_FRAMES = 10;
     
-    public static final int MAX_FILE_DOWNLOAD_PER_BATCH = 5;
+    public static final int MAX_FILE_DOWNLOAD_PER_BATCH = 1500;
 
 }
