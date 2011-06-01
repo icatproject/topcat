4f34bc18a01c4a6fcd6ebc6ca21159482abebb51
Antony Wilson <antony.wilson@stfc.ac.uk>
2011-06-01 09:10:36 +0000

    format

--- Code/TopCAT/src/main/java/uk/ac/stfc/topcat/gwt/client/widget/DatafileWindow.java
+++ Code/TopCAT/src/main/java/uk/ac/stfc/topcat/gwt/client/widget/DatafileWindow.java
@@ -21,6 +21,7 @@
  * OF SUCH DAMAGE.
  */
 package uk.ac.stfc.topcat.gwt.client.widget;
+
 /**
  * Imports
  */
@@ -42,6 +43,8 @@ import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.GridEvent;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.event.WindowEvent;
+import com.extjs.gxt.ui.client.event.WindowListener;
 import com.extjs.gxt.ui.client.store.GroupingStore;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
@@ -61,198 +64,219 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import com.extjs.gxt.ui.client.event.WindowListener;
-import com.extjs.gxt.ui.client.event.WindowEvent;
 
 /**
- * This is a floating window widget, It shows list of datafiles for a given investigation. 
+ * This is a floating window widget, It shows list of datafiles for a given
+ * investigation.
  * 
  * <p>
+ * 
  * @author Mr. Srikanth Nagella
- * @version 1.0,  &nbsp; 30-APR-2010
- * @since iCAT Version 3.3   
+ * @version 1.0, &nbsp; 30-APR-2010
+ * @since iCAT Version 3.3
  */
 public class DatafileWindow extends Window {
-	private final UtilityServiceAsync utilityService = GWT.create(UtilityService.class);	
-	ParameterWindow datafileInfoWindow;
-	CheckBoxSelectionModel<DatafileModel> datafileSelectModel;
-	GroupingStore<DatafileModel> dfmStore;
-	ArrayList<DatasetModel> inputDatasetModels;
-	boolean historyVerified;
-        boolean hasData;
-	public DatafileWindow() {
-		addWindowListener(new WindowListener() {
-			public void windowHide(WindowEvent we) {
-//				EventPipeLine.getInstance().removeWindowHistory(getHistoryString());
-				EventPipeLine.getInstance().getHistoryManager().updateHistory();
-			}
-		});
-		dfmStore = new GroupingStore<DatafileModel>();	
-		datafileSelectModel = new CheckBoxSelectionModel<DatafileModel>();		
-		dfmStore.groupBy("datasetName");		
-		datafileInfoWindow = new ParameterWindow();				
-		setHeading("Datafile Window");
-		setLayout(new RowLayout(Orientation.VERTICAL));
-		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+    private final UtilityServiceAsync utilityService = GWT.create(UtilityService.class);
+    ParameterWindow datafileInfoWindow;
+    CheckBoxSelectionModel<DatafileModel> datafileSelectModel;
+    GroupingStore<DatafileModel> dfmStore;
+    ArrayList<DatasetModel> inputDatasetModels;
+    boolean historyVerified;
+    boolean hasData;
 
-		configs.add(datafileSelectModel.getColumn());
-		
-		ColumnConfig clmncnfgDatasetName = new ColumnConfig("datasetName", "Dataset Name", 150);
-		configs.add(clmncnfgDatasetName);
-		
-		ColumnConfig clmncnfgFileName = new ColumnConfig("datafileName", "File Name", 150);
-		configs.add(clmncnfgFileName);
-		
-		ColumnConfig clmncnfgFileSizeb = new ColumnConfig("datafileSize", "File Size (MBytes)", 150);
-		configs.add(clmncnfgFileSizeb);
-		
-		ColumnConfig clmncnfgFormat = new ColumnConfig("datafileFormat", "Format", 150);
-		configs.add(clmncnfgFormat);
-		
-		ColumnConfig clmncnfgFormatVersion = new ColumnConfig("datafileFormatVersion", "Format Version", 150);
-		configs.add(clmncnfgFormatVersion);
-		
-		ColumnConfig clmncnfgFormatType = new ColumnConfig("datafileFormatType", "Format Type", 150);
-		configs.add(clmncnfgFormatType);
-		
-		ColumnConfig clmncnfgCreateTime = new ColumnConfig("datafileCreateTime", "Create Time", 150);
-		clmncnfgCreateTime.setDateTimeFormat(DateTimeFormat.getShortDateTimeFormat());
-		configs.add(clmncnfgCreateTime);
-		
-		final ColumnModel cm = new ColumnModel(configs);
-		GroupingView view = new GroupingView();    
-	    view.setShowGroupedColumn(false);		
-		view.setForceFit(true);    
-		view.setGroupRenderer(new GridGroupRenderer() {    
-			@Override
-			public String render(GroupColumnData data) {    
-				String f = cm.getColumnById(data.field).getHeader();    
-				String l = data.models.size() == 1 ? "Item" : "Items";    
-				return f + ": " + data.group + " (" + data.models.size() + " " + l + ")";    
-			}  
-		});		
-		Grid<DatafileModel> grid = new Grid<DatafileModel>(dfmStore, cm);
-		grid.setHeight("398px");
-		grid.setView(view);
-		grid.setBorders(true);
-		grid.addListener(Events.RowDoubleClick, new Listener<GridEvent<DatafileModel>>() {
-			public void handleEvent(GridEvent<DatafileModel> e) {
-				DatafileModel datafile = (DatafileModel)e.getModel();
-				EventPipeLine.getInstance().showParameterWindowWithHistory(datafile.getFacilityName(), datafile.getId(), datafile.getName());
-			}
-		});		
-		grid.addPlugin(datafileSelectModel);
-		grid.setSelectionModel(datafileSelectModel);
-	    ToolBar toolBar = new ToolBar();
-	    Button  btnView = new Button(" Download", AbstractImagePrototype.create(Resource.ICONS.iconDownload())); 
-	    btnView.addSelectionListener(new SelectionListener<ButtonEvent>() {
-	    	public void componentSelected(ButtonEvent ce) {
-	    	    download();
-	    	}
-	    });
-	    toolBar.add(btnView);
-	    toolBar.add(new SeparatorToolItem());
-	    setTopComponent(toolBar);	   
-	    
-	    setLayout(new FitLayout());
-	    setSize(700,500);
-	    add(grid);
-            hasData=true;
-	}
-	
-	/**
-	 * Set the datasets input which are used to get the datafiles corresponding to each datasets and are displayed
-	 * in the window
-	 * @param datasetList
-	 */
-	public void setDatasets(ArrayList<DatasetModel> datasetList) {
-		inputDatasetModels=datasetList;
-		//This is the list of datasets selected to be viewed for datafiles.
-		utilityService.getDatafilesInDatasets(datasetList, new AsyncCallback<ArrayList<DatafileModel>>() {
-			@Override
-			public void onSuccess(ArrayList<DatafileModel> result) {
-				setDatafileList(result);
-                                hasData=true;
-			}
-			
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				dfmStore.removeAll();
-                                hasData=false;
-			}
-		});
-	}
-	
-	/**
-	 * Set the datafile list to be displayed in the window
-	 * @param datafileList
-	 */
-	public void setDatafileList(ArrayList<DatafileModel> datafileList){
-		dfmStore.removeAll();
-		NumberFormat format = NumberFormat.getDecimalFormat();
-		//convert Bytes to MegaBytes
-		for(DatafileModel dfm :datafileList){
-			Float size = Float.parseFloat(dfm.getFileSize());
-			size=size/1048576.0f;
-			dfm.setFileSize(format.format(size.doubleValue())+" MB");
-		}
-		dfmStore.add(datafileList);
-	}
-
-	/**
-	 * @return the history string of this window
-	 */
-	public String getHistoryString() {
-		String history="";
-		history+=HistoryManager.seperatorModel+HistoryManager.seperatorToken+"Model"+HistoryManager.seperatorKeyValues+"Dataset";
-		int count=0;
-		for(DatasetModel dataset:inputDatasetModels){
-			history+=HistoryManager.seperatorToken+"SN-"+count+HistoryManager.seperatorKeyValues+dataset.getFacilityName();		
-			history+=HistoryManager.seperatorToken+"DSId-"+count+HistoryManager.seperatorKeyValues+dataset.getId();
-			history+=HistoryManager.seperatorToken+"DSName-"+count+HistoryManager.seperatorKeyValues+dataset.getName();
-			count++;
-		}
-		return history;
-	}
-
-	/**
-	 * This method compares the input list of datasetmodes, If they match with current windows
-	 * datasetmodels then it returns true otherwise false
-	 * @param dsModelList
-	 * @return
-	 */
-	public boolean isSameModel(ArrayList<DatasetModel> dsModelList) {
-		int index=0;
-		for(DatasetModel dsModel:dsModelList){			
-			if(dsModel.getFacilityName().compareTo(inputDatasetModels.get(index).getFacilityName())!=0 || dsModel.getId().compareTo(inputDatasetModels.get(index).getId())!=0 ) return false;
-			index++;
-		}
-		return true;
-	}
-	
-	/**
-	 * @return returns the history verified status
-	 */
-	public boolean isHistoryVerified() {
-		return historyVerified;
-	}
-	
-	/**
-	 * This method sets the history verified status
-	 * @param historyVerified
-	 */
-	public void setHistoryVerified(boolean historyVerified) {
-		this.historyVerified = historyVerified;
-	}
-
-        @Override
-        public void show(){
-            if(!hasData){
-                setDatasets(inputDatasetModels);
+    public DatafileWindow() {
+        addWindowListener(new WindowListener() {
+            public void windowHide(WindowEvent we) {
+                // EventPipeLine.getInstance().removeWindowHistory(getHistoryString());
+                EventPipeLine.getInstance().getHistoryManager().updateHistory();
             }
-            super.show();
+        });
+        dfmStore = new GroupingStore<DatafileModel>();
+        datafileSelectModel = new CheckBoxSelectionModel<DatafileModel>();
+        dfmStore.groupBy("datasetName");
+        datafileInfoWindow = new ParameterWindow();
+        setHeading("Datafile Window");
+        setLayout(new RowLayout(Orientation.VERTICAL));
+        List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+
+        configs.add(datafileSelectModel.getColumn());
+
+        ColumnConfig clmncnfgDatasetName = new ColumnConfig("datasetName", "Dataset Name", 150);
+        configs.add(clmncnfgDatasetName);
+
+        ColumnConfig clmncnfgFileName = new ColumnConfig("datafileName", "File Name", 150);
+        configs.add(clmncnfgFileName);
+
+        ColumnConfig clmncnfgFileSizeb = new ColumnConfig("datafileSize", "File Size (MBytes)", 150);
+        configs.add(clmncnfgFileSizeb);
+
+        ColumnConfig clmncnfgFormat = new ColumnConfig("datafileFormat", "Format", 150);
+        configs.add(clmncnfgFormat);
+
+        ColumnConfig clmncnfgFormatVersion = new ColumnConfig("datafileFormatVersion", "Format Version", 150);
+        configs.add(clmncnfgFormatVersion);
+
+        ColumnConfig clmncnfgFormatType = new ColumnConfig("datafileFormatType", "Format Type", 150);
+        configs.add(clmncnfgFormatType);
+
+        ColumnConfig clmncnfgCreateTime = new ColumnConfig("datafileCreateTime", "Create Time", 150);
+        clmncnfgCreateTime.setDateTimeFormat(DateTimeFormat.getShortDateTimeFormat());
+        configs.add(clmncnfgCreateTime);
+
+        final ColumnModel cm = new ColumnModel(configs);
+        GroupingView view = new GroupingView();
+        view.setShowGroupedColumn(false);
+        view.setForceFit(true);
+        view.setGroupRenderer(new GridGroupRenderer() {
+            @Override
+            public String render(GroupColumnData data) {
+                String f = cm.getColumnById(data.field).getHeader();
+                String l = data.models.size() == 1 ? "Item" : "Items";
+                return f + ": " + data.group + " (" + data.models.size() + " " + l + ")";
+            }
+        });
+        Grid<DatafileModel> grid = new Grid<DatafileModel>(dfmStore, cm);
+        grid.setHeight("398px");
+        grid.setView(view);
+        grid.setBorders(true);
+        grid.addListener(Events.RowDoubleClick, new Listener<GridEvent<DatafileModel>>() {
+            public void handleEvent(GridEvent<DatafileModel> e) {
+                DatafileModel datafile = (DatafileModel) e.getModel();
+                EventPipeLine.getInstance().showParameterWindowWithHistory(datafile.getFacilityName(),
+                        datafile.getId(), datafile.getName());
+            }
+        });
+        grid.addPlugin(datafileSelectModel);
+        grid.setSelectionModel(datafileSelectModel);
+        ToolBar toolBar = new ToolBar();
+        Button btnView = new Button(" Download", AbstractImagePrototype.create(Resource.ICONS.iconDownload()));
+        btnView.addSelectionListener(new SelectionListener<ButtonEvent>() {
+            public void componentSelected(ButtonEvent ce) {
+                download();
+            }
+        });
+        toolBar.add(btnView);
+        toolBar.add(new SeparatorToolItem());
+        setTopComponent(toolBar);
+
+        setLayout(new FitLayout());
+        setSize(700, 500);
+        add(grid);
+        hasData = true;
+    }
+
+    /**
+     * Set the datasets input which are used to get the datafiles corresponding
+     * to each dataset and are displayed in the window.
+     * 
+     * @param datasetList
+     *            an array of <code>DatasetModel</code>
+     */
+    public void setDatasets(ArrayList<DatasetModel> datasetList) {
+        inputDatasetModels = datasetList;
+        // This is the list of datasets selected to be viewed for datafiles.
+        utilityService.getDatafilesInDatasets(datasetList, new AsyncCallback<ArrayList<DatafileModel>>() {
+            @Override
+            public void onSuccess(ArrayList<DatafileModel> result) {
+                setDatafileList(result);
+                hasData = true;
+            }
+
+            @Override
+            public void onFailure(Throwable caught) {
+                // TODO Auto-generated method stub
+                dfmStore.removeAll();
+                hasData = false;
+            }
+        });
+    }
+
+    /**
+     * Set the datafile list to be displayed in the window.
+     * 
+     * @param datafileList
+     *            an array of <code>DatafileModel</code>
+     */
+    public void setDatafileList(ArrayList<DatafileModel> datafileList) {
+        dfmStore.removeAll();
+        NumberFormat format = NumberFormat.getDecimalFormat();
+        // convert Bytes to MegaBytes
+        for (DatafileModel dfm : datafileList) {
+            Float size = Float.parseFloat(dfm.getFileSize());
+            size = size / 1048576.0f;
+            dfm.setFileSize(format.format(size.doubleValue()) + " MB");
         }
+        dfmStore.add(datafileList);
+    }
+
+    /**
+     * Get the history of this window.
+     * 
+     * @return the history string of this window
+     */
+    public String getHistoryString() {
+        String history = "";
+        history += HistoryManager.seperatorModel + HistoryManager.seperatorToken + "Model"
+                + HistoryManager.seperatorKeyValues + "Dataset";
+        int count = 0;
+        for (DatasetModel dataset : inputDatasetModels) {
+            history += HistoryManager.seperatorToken + "SN-" + count + HistoryManager.seperatorKeyValues
+                    + dataset.getFacilityName();
+            history += HistoryManager.seperatorToken + "DSId-" + count + HistoryManager.seperatorKeyValues
+                    + dataset.getId();
+            history += HistoryManager.seperatorToken + "DSName-" + count + HistoryManager.seperatorKeyValues
+                    + dataset.getName();
+            count++;
+        }
+        return history;
+    }
+
+    /**
+     * Compares the input list of <code>DatasetModel</code>s with the
+     * <code>DatasetModel</code>s in the current windows.
+     * 
+     * @param dsModelList
+     *            array of <code>DatasetModel</code>
+     * @return <code>true</code> if input list matches current windows
+     *         <code>DatasetModel</code>s
+     */
+    public boolean isSameModel(ArrayList<DatasetModel> dsModelList) {
+        int index = 0;
+        for (DatasetModel dsModel : dsModelList) {
+            if (dsModel.getFacilityName().compareTo(inputDatasetModels.get(index).getFacilityName()) != 0
+                    || dsModel.getId().compareTo(inputDatasetModels.get(index).getId()) != 0)
+                return false;
+            index++;
+        }
+        return true;
+    }
+
+    /**
+     * Check if the history is verified.
+     * 
+     * @return returns the history verified status
+     */
+    public boolean isHistoryVerified() {
+        return historyVerified;
+    }
+
+    /**
+     * Set the history verified status.
+     * 
+     * @param historyVerified
+     *            history verified status
+     */
+    public void setHistoryVerified(boolean historyVerified) {
+        this.historyVerified = historyVerified;
+    }
+
+    @Override
+    public void show() {
+        if (!hasData) {
+            setDatasets(inputDatasetModels);
+        }
+        super.show();
+    }
 
     /**
      * Download selected datafiles.
