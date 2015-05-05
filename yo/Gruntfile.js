7358d8012de915432952e4f3a1ae1f937562fd2f
Wayne Chung <wayne.chung@stfc.ac.uk>
2015-05-05 13:05:13 +0000

    Fixed issue where datatables images were not copied when running grunt build

--- yo/Gruntfile.js
+++ yo/Gruntfile.js
@@ -281,6 +281,12 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>/images',
           src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/images'
+        },
+        {
+          expand: true,
+          cwd: 'bower_components/datatables/media/images',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= yeoman.dist %>/images'
         }]
       }
     },
