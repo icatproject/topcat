3a2d0952c8dd373f67fe1e325a86a88574a48738
Jody Salt <jody.salt@stfc.ac.uk>
2015-10-19 10:43:37 +0100

    added titles to breadcrumb #69

--- yo/app/scripts/directives/breadcrumb.directive.js
+++ yo/app/scripts/directives/breadcrumb.directive.js
@@ -4,27 +4,63 @@
     angular.
         module('angularApp').controller('BreadCrumbController', BreadCrumbController);
 
-    BreadCrumbController.$inject = ['$scope', '$state', '$stateParams', 'RouteService'];
+    BreadCrumbController.$inject = ['$scope', '$state', '$stateParams', '$sessionStorage', '$q', 'Config', 'RouteService', 'APP_CONFIG', 'ICATService'];
 
-    function BreadCrumbController ($scope, $state, $stateParams, RouteService) {
+    function BreadCrumbController ($scope, $state, $stateParams, $sessionStorage, $q, Config, RouteService, APP_CONFIG, ICATService) {
         var bc = this;
         var previousRoutes = RouteService.getPreviousRoutes($state);
-
+        var sessionId = $stateParams.facilityName ? $sessionStorage.sessions[$stateParams.facilityName].sessionId : null;
+        var facility = Config.getFacilityByName(APP_CONFIG, $stateParams.facilityName);
+        var promises = [];
         var items = [];
 
-        _.each(previousRoutes, function(route){
-            var item = {
-                translate: 'ENTITIES.' + route.entity.toUpperCase() + '.NAME'
-            };
+        var titles = {};
+        if(facility){
+            titles.facility = facility.title;
+        }
+        if($stateParams.proposalId){
+            titles.proposal = $stateParams.proposalId;
+        }
+        if($stateParams.investigationId){
+            promises.push(ICATService.getEntityById(
+                sessionId,
+                facility,
+                'Investigation',
+                $stateParams.investigationId
+            ).success(function(data){
+                titles.investigation = data[0].Investigation.title;
+            }));
+        }
+        if($stateParams.datasetId){
+            promises.push(ICATService.getEntityById(
+                sessionId,
+                facility,
+                'Dataset',
+                $stateParams.datasetId
+            ).success(function(data){
+                titles.dataset = data[0].Dataset.name;
+            }));
+        }
+        
+        
+        $q.all(promises).then(function(){
+            _.each(previousRoutes, function(route){
+                var item = {
+                    translate: 'ENTITIES.' + route.entity.toUpperCase() + '.NAME',
+                    title: titles[route.entity]
+                };
 
-            //special case for facility
-            if (route.route === 'facility') {
-                item.route = 'home.browse.facility('+ JSON.stringify($stateParams) + ')';
-            } else {
-                item.route = 'home.browse.facility.' + route.route + '('+ JSON.stringify($stateParams) + ')';
-            }
+                //special case for facility
+                if (route.route === 'facility') {
+                    item.route = 'home.browse.facility('+ JSON.stringify($stateParams) + ')';
+                } else {
+                    item.route = 'home.browse.facility.' + route.route + '('+ JSON.stringify($stateParams) + ')';
+                }
 
-            items.push(item);
+                items.push(item);
+            });
+
+            bc.items = items;
         });
 
         bc.items = items;
@@ -43,7 +79,7 @@
                 //@ reads the attribute value, = provides two-way binding, & works with functions
                 items: '@'
             },
-            template: '<ul class="breadcrumb"><li ng-repeat="item in bc.items"><span ng-show="! $last"><a ui-sref="{{ item.route }}" translate="{{ item.translate }}"></a></span><span ng-show="$last" translate="{{ item.translate }}"></span></li></ul>',
+            template: '<ul class="breadcrumb"><li ng-repeat="item in bc.items"><span ng-show="! $last"><a ui-sref="{{ item.route }}">{{item.title}}</a></span><span ng-show="$last" translate="{{ item.translate }}"></span></li></ul>',
             //templateUrl: 'views/breadcrumb.directive.html',
             controller: 'BreadCrumbController',
             controllerAs: 'bc'
