1c6e34f80aa9a6df69e1ca9c4e493682125af797
Jody Salt <jody.salt@stfc.ac.uk>
2015-11-26 17:01:08 +0000

    styled facility selection tickboxes #74

--- yo/app/scripts/controllers/search-results.controller.js
+++ yo/app/scripts/controllers/search-results.controller.js
@@ -7,6 +7,8 @@
     	var facilities = $stateParams.facilities ? JSON.parse($stateParams.facilities) : [];
     	var text = $stateParams.text;
     	var type = $stateParams.type;
+    	var startDate = $stateParams.startDate;
+    	var endDate = $stateParams.endDate;
 
         var gridOptions = {data: []};
         _.merge(gridOptions, APP_CONFIG.site.searchGridOptions[type]);
@@ -14,8 +16,13 @@
 
      	var query = {target: type}
      	if(text) query.text = text;
+     	if(startDate) query.lower = startDate.replace(/-/g, '') + "0000";
+     	if(endDate) query.upper = endDate.replace(/-/g, '') + "0000";
+
+     	console.log('query', query);
 
         ICATSearchService.search(facilities, query, function(results){
+        	console.log('results', results);
         	gridOptions.data = _.map(results, function(result){
         		return result[type];
         	});
