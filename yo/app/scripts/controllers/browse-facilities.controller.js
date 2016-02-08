8028ed2d0a752fa825128ad9e06b2eac00622c53
Jody Salt <jody.salt@stfc.ac.uk>
2016-02-08 11:20:39 +0000

    refactored browse facilities controller #176

--- yo/app/scripts/controllers/browse-facilities.controller.js
+++ yo/app/scripts/controllers/browse-facilities.controller.js
@@ -5,7 +5,7 @@
 
     var app = angular.module('angularApp');
 
-    app.controller('BrowseFacilitiesController', function($state, $q, $scope, $rootScope, $timeout, tc){
+    app.controller('BrowseFacilitiesController', function($state, $q, $scope, $rootScope, $timeout, tc, uiGridConstants){
         var pagingConfig = tc.config().paging;
         var isScroll = pagingConfig.pagingType == 'scroll';
         var pageSize = isScroll ? pagingConfig.scrollPageSize : pagingConfig.paginationNumberOfRows;
@@ -13,6 +13,28 @@
         this.gridOptions = gridOptions;
         this.isScroll = isScroll;
         gridOptions.data = [];
+
+        if(tc.userFacilities().length == 1){
+            var facility = tc.userFacilities()[0];
+            var hierarchy = facility.config().hierarchy;
+            $state.go('home.browse.facility.' + _.slice(hierarchy, 0, 2).join('-'), {
+                facilityName: facility.config().facilityName
+            });
+            return;
+        }
+
+        _.each(gridOptions.columnDefs, function(columnDef) {
+            if(columnDef.filter && columnDef.filter.condition) {
+                columnDef.filter.condition = uiGridConstants.filter[columnDef.filter.condition.toUpperCase()];
+            }
+            if(columnDef.translateDisplayName) {
+                columnDef.displayName = columnDef.translateDisplayName;
+                columnDef.headerCellFilter = 'translate';
+            }
+            if(columnDef.link === true) {
+                columnDef.cellTemplate = columnDef.cellTemplate || '<div class="ui-grid-cell-contents" title="TOOLTIP"><a ng-click="$event.stopPropagation();" href="{{grid.appScope.getNextRouteUrl(row.entity)}}">{{row.entity.' + columnDef.field + '}}</a></div>';
+            }
+        });
 
         _.map(tc.userFacilities(), function(facility){
             facility.icat().entity("Facility", ["where facility.id = ?", facility.config().facilityId]).then(function(facility){
@@ -22,15 +44,15 @@
 
 
         this.getNextRouteUrl = function(facility){
-            var hierarchy = facility.config().hierarchy
-            var stateSuffixes = {};
-            _.each(hierarchy, function(currentEntityType, i){
-                stateSuffixes[currentEntityType] = _.slice(hierarchy, 0, i + 2).join('-');
-            });
-            var params = _.clone($state.params);
-            delete params.uiGridState;
-            params[entityInstanceName + 'Id'] = row.id || row.name;
-            return $state.href('home.browse.facility.' + stateSuffixes[entityInstanceName], params);
+            facility = _.select(tc.facilities(), function(_facility){
+                return _facility.config().facilityId == facility.id;
+            })[0];
+            if(facility){
+                var hierarchy = facility.config().hierarchy;
+                return $state.href('home.browse.facility.' + _.slice(hierarchy, 0, 2).join('-'), {
+                    facilityName: facility.config().facilityName
+                });
+            }
         };
 
 
