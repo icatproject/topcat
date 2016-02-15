a3880fe997d9e288ab41ea1eaffe37ce278c994a
Jody Salt <jody.salt@stfc.ac.uk>
2016-02-15 16:29:43 +0000

    grids now take up remaining space - close #101

--- yo/app/scripts/directives/take-up-remaining-height.directive.js
+++ yo/app/scripts/directives/take-up-remaining-height.directive.js
@@ -4,21 +4,49 @@
     'use strict';
 
     var app = angular.module('angularApp');
+    var lastHeight;
 
     app.directive('takeUpRemainingHeight', function(){
         return {
             restrict: 'A',
-            controller: function($element, $timeout){
-                $timeout(function(){
-                    var height = $($element).parent().innerHeight();
-                    $($element).parent().children().each(function(){
-                        if(this != $element[0]) {
-                            height = height - $(this).outerHeight();
-                        }
+            controller: function($element, $timeout, $scope){
+
+                if(lastHeight){
+                    $($element).css('height', lastHeight + 'px');
+                }
+
+                function refresh(){
+                    var height;
+                    var viewport = $($element).parents('.modal-content').first();
+                    if(viewport.length > 0){
+                        height = $(viewport).height();
+                    } else {
+                        height = $(window).height();
+                        viewport = document.body;
+                    }
+
+                    var heightInUse = 0;
+                    $(viewport).children().each(function(i, element){
+                        if($(element).css('z-index') != 'auto') return;
+                        heightInUse = heightInUse + $(element).outerHeight();
                     });
-                    $($element).css('height', height + 'px');
+
+                    var remainingHeight = height - heightInUse;
+
+
+                    $($element).css('height', $($element).height() + remainingHeight + 'px');
+                    lastHeight = $($element).height();
+
+                    if(remainingHeight !== 0){
+                        $($element).trigger('resize');
+                    }
+                }
+
+                var interval = setInterval(refresh, 50);
+
+                $scope.$on('$destroy', function(){
+                    clearInterval(interval);
                 });
-                
             }
         };
     });
