515473a19267c965527dd16845a1d51ccb1665cc
Jody Salt <jody.salt@stfc.ac.uk>
2016-02-19 13:30:56 +0000

    removed some duplication

--- yo/app/scripts/controllers/admin.controller.js
+++ yo/app/scripts/controllers/admin.controller.js
@@ -4,7 +4,7 @@
 
     var app = angular.module('angularApp');
 
-    app.controller('AdminController', function($translate, $scope, $state, $timeout, $q, tc){
+    app.controller('AdminController', function($translate, $scope, $state, $timeout, $q, tc, helpers){
     	var that = this;
       var page = 1;
       var gridApi;
@@ -87,8 +87,8 @@
                   var from = columnDef.filters[0].term || '';
                   var to = columnDef.filters[1].term || '';
                   if(from != '' || to != ''){
-                      from = completePartialFromDate(from);
-                      to = completePartialToDate(to);
+                      from = helpers.completePartialFromDate(from);
+                      to = helpers.completePartialToDate(to);
                       filters.push([
                           "and download.? between {ts ?} and {ts ?}",
                           columnDef.field.safe(),
@@ -105,50 +105,6 @@
               }
           });
       }
-
-      function completePartialFromDate(date){
-            var segments = date.split(/[-:\s]+/);
-            var year = segments[0];
-            var month = segments[1] || "01";
-            var day = segments[2] || "01";
-            var hours = segments[3] || "00";
-            var minutes = segments[4] || "00";
-            var seconds = segments[5] || "00";
-
-            year = year + '0000'.slice(year.length, 4);
-            month = month + '00'.slice(month.length, 2);
-            day = day + '00'.slice(day.length, 2);
-            hours = hours + '00'.slice(hours.length, 2);
-            minutes = minutes + '00'.slice(minutes.length, 2);
-            seconds = seconds + '00'.slice(seconds.length, 2);
-
-            if(parseInt(month) == 0) month = '01';
-            if(parseInt(day) == 0) day = '01';
-
-            return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
-        }
-
-        function completePartialToDate(date){
-            var segments = date.split(/[-:\s]+/);
-            var year = segments[0] || "";
-            var month = segments[1] || "";
-            var day = segments[2] || "";
-            var hours = segments[3] || "23";
-            var minutes = segments[4] || "59";
-            var seconds = segments[5] || "59";
-            year = year + '9999'.slice(year.length, 4);
-            month = month + '99'.slice(month.length, 2);
-            day = day + '99'.slice(day.length, 2);
-            hours = hours + '33'.slice(hours.length, 2);
-            minutes = minutes + '99'.slice(minutes.length, 2);
-            seconds = seconds + '99'.slice(seconds.length, 2);
-
-            if(parseInt(month) > 12) month = '12';
-            var daysInMonth = new Date(year, day, 0).getDate();
-            if(parseInt(day) > daysInMonth) day = daysInMonth;
-
-            return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
-        }
 
       this.pause = function(download){
         admin.setDownloadStatus(download.id, 'PAUSED').then(function(){
--- yo/app/scripts/controllers/my-data.controller.js
+++ yo/app/scripts/controllers/my-data.controller.js
@@ -4,7 +4,7 @@
 
     var app = angular.module('angularApp');
 
-    app.controller('MyDataController', function($translate, $q, $scope, $rootScope, $timeout, $templateCache, $state, tc, uiGridConstants){
+    app.controller('MyDataController', function($translate, $q, $scope, $rootScope, $timeout, $templateCache, $state, tc, helpers, uiGridConstants){
         var that = this;
         var pagingConfig = tc.config().paging;
         var entityType = tc.config().myDataGridOptions.entityType;
@@ -171,8 +171,8 @@
                     var from = columnDef.filters[0].term || '';
                     var to = columnDef.filters[1].term || '';
                     if(from != '' || to != ''){
-                        from = completePartialFromDate(from);
-                        to = completePartialToDate(to);
+                        from = helpers.completePartialFromDate(from);
+                        to = helpers.completePartialToDate(to);
                         filterQuery.push([
                             "and ? between {ts ?} and {ts ?}",
                             columnDef.jpqlExpression.safe(),
@@ -188,50 +188,6 @@
                     ]);
                 }
             });
-        }
-
-        function completePartialFromDate(date){
-            var segments = date.split(/[-:\s]+/);
-            var year = segments[0];
-            var month = segments[1] || "01";
-            var day = segments[2] || "01";
-            var hours = segments[3] || "00";
-            var minutes = segments[4] || "00";
-            var seconds = segments[5] || "00";
-
-            year = year + '0000'.slice(year.length, 4);
-            month = month + '00'.slice(month.length, 2);
-            day = day + '00'.slice(day.length, 2);
-            hours = hours + '00'.slice(hours.length, 2);
-            minutes = minutes + '00'.slice(minutes.length, 2);
-            seconds = seconds + '00'.slice(seconds.length, 2);
-
-            if(parseInt(month) == 0) month = '01';
-            if(parseInt(day) == 0) day = '01';
-
-            return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
-        }
-
-        function completePartialToDate(date){
-            var segments = date.split(/[-:\s]+/);
-            var year = segments[0] || "";
-            var month = segments[1] || "";
-            var day = segments[2] || "";
-            var hours = segments[3] || "23";
-            var minutes = segments[4] || "59";
-            var seconds = segments[5] || "59";
-            year = year + '9999'.slice(year.length, 4);
-            month = month + '99'.slice(month.length, 2);
-            day = day + '99'.slice(day.length, 2);
-            hours = hours + '33'.slice(hours.length, 2);
-            minutes = minutes + '99'.slice(minutes.length, 2);
-            seconds = seconds + '99'.slice(seconds.length, 2);
-
-            if(parseInt(month) > 12) month = '12';
-            var daysInMonth = new Date(year, day, 0).getDate();
-            if(parseInt(day) > daysInMonth) day = daysInMonth;
-
-            return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
         }
 
         function updateScroll(resultCount){
