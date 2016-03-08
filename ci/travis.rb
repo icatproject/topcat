b69ba216675ef00948f0291c25f6039b6c7de06c
Jody Salt <jody@jodysalt.com>
2016-03-08 16:54:50 +0000

    updated ci

--- ci/travis.rb
+++ ci/travis.rb
@@ -1,12 +1,8 @@
 #!/usr/bin/env ruby
 
-commands = (dirs.map do |dir|
-  %{
-    echo "create database icat;" | mysql -u root --password=secret
-    echo "create database topcat;" | mysql -u root --password=secret
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION" | mysql -u
-    
-  }
-end).map{ |lines| lines.strip.split(/\n/).join(' && ') }.join(" && ")
+exec %{
+  echo "create database icat;" | mysql -u root --password=secret
+  echo "create database topcat;" | mysql -u root --password=secret
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION" | mysql -u
+}.split(/\n/).join(' && ')
 
-exec commands
