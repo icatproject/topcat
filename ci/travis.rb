2ebfc9dbafdb738eb75a07a640b68221f19bce16
Jody Salt <jody.salt@stfc.ac.uk>
2016-03-10 15:29:10 +0000

    updated ci

--- ci/travis.rb
+++ ci/travis.rb
@@ -111,9 +111,7 @@ exec %{
   sudo /opt/glassfish4/bin/asadmin -t set applications.application.topcat-2.0.0-SNAPSHOT.deployment-order=140
 
   mysql -u root --password=secret --host=127.0.0.1 icat < ./provision/icat.sql
-  sudo apt-get --assume-yes  install python-pip apt-get libffi-dev libssl-dev
-  sudo pip install --upgrade pip
-  sudo pip install requests[security]
+  sudo apt-get --assume-yes  install python-pip
   sudo pip install suds
   cd ./icat.server
   ./icatadmin https://localhost:8181 simple username root password root -- populate
