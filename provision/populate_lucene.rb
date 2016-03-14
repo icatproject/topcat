a90d0786b9507a989fb16b8a8ae22ce8be7bfed9
Jody Salt <jody.salt@stfc.ac.uk>
2016-03-14 09:42:30 +0000

    updated ci

--- provision/populate_lucene.rb
+++ provision/populate_lucene.rb
@@ -3,21 +3,28 @@ require 'rest-client'
 require 'openssl'
 require 'json'
 
-sessionId = JSON.parse(RestClient.post 'https://localhost:8181/icat/session/', :json => JSON.generate({
-	:plugin => "simple",
-	:credentials => [
-		{:username => "root"},
-		{:password => "root"}
-	]
-}, {
+
+
+sessionId = JSON.parse(RestClient::Request.execute({
+	:method => :post,
+	:url => 'https://localhost:8181/icat/session/',
+	:payload => {
+		:json => JSON.generate({
+			:plugin => "simple",
+			:credentials => [
+				{:username => "root"},
+				{:password => "root"}
+			]
+		})
+	},
 	:verify_ssl => OpenSSL::SSL::VERIFY_NONE
 }))['sessionId']
 
-
 ['Investigation', 'Dataset', 'Datafile'].each do |entityName|
-	RestClient.post("https://localhost:8181/icat/lucene/db/#{entityName}/", {
-		:sessionId => sessionId
-	}, {
+	RestClient::Request.execute({
+		:method => :post,
+		:url => "https://localhost:8181/icat/lucene/db/#{entityName}/",
+		:payload => {:sessionId => sessionId},
 		:verify_ssl => OpenSSL::SSL::VERIFY_NONE
 	})
 end
