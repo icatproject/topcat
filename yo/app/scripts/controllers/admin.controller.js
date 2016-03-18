2b9274d78ac6fa4c740e604aca1c6c4bbeadf31c
Jody Salt <jody.salt@stfc.ac.uk>
2016-03-18 16:06:38 +0000

    refactor admin is now fixed

--- yo/app/scripts/controllers/admin.controller.js
+++ yo/app/scripts/controllers/admin.controller.js
@@ -16,6 +16,7 @@
       });
 
     	this.facilities = tc.adminFacilities();
+
       if($state.params.facilityName == ''){
           $state.go('admin', {facilityName: this.facilities[0].config().facilityName});
           return;
