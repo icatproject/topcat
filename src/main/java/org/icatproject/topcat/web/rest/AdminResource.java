d4922248fe1a2ac399022c3834974a9c1c6982eb
Jody Salt <jody.salt@stfc.ac.uk>
2016-03-30 11:43:17 +0100

    created some REST API documentation

--- src/main/java/org/icatproject/topcat/web/rest/AdminResource.java
+++ src/main/java/org/icatproject/topcat/web/rest/AdminResource.java
@@ -77,9 +77,12 @@ public class AdminResource {
      *
      * @param queryOffset
      *  any JPQL expression that can be appended to "SELECT download from Download download",
-     *  e.g. "where dowload.isDeleted = false". Note that like ICAT the syntax has been extended
+     *  e.g. "where download.isDeleted = false". Note that like ICAT the syntax has been extended
      *  allowing (sql like) limit clauses in the form "limit [offset], [row count]" e.g. "limit 10, 20".
-     *  
+     *  Note the "status" attribute is an enum (not a string) i.e. org.icatproject.topcat.domain.Status
+     *  with the following possible states: ONLINE, ARCHIVE or RESTORING. So an example query involving
+     *  the status attribute could be "where download.status = org.icatproject.topcat.domain.Status.ARCHIVE limit 0, 10"
+     *
      * @return returns an array of downloads in the form
      * [{"completedAt":"2016-03-18T16:02:36","createdAt":"2016-03-18T16:02:36","deletedAt":"2016-03-18T16:02:47","downloadItems":[{"entityId":18064,"entityType":"datafile","id":2},{"entityId":18061,"entityType":"datafile","id":3}],"email":"","facilityName":"test","fileName":"test_2016-3-18_16-05-59","icatUrl":"https://example.com","id":2,"isDeleted":false,"isTwoLevel":false,"preparedId":"6d3aaca5-da9f-4e6a-922d-eceeefcc07e0","status":"COMPLETE","transport":"https","transportUrl":"https://example.com","userName":"simple/root"}]
      */
@@ -104,7 +107,15 @@ public class AdminResource {
     }
 
     /**
+     * Sets the download status.
+     *
+     * @param icatUrl a url to a valid ICAT REST api.
      * 
+     * @param sessionId a valid session id which takes the form <code>0d9a3706-80d4-4d29-9ff3-4d65d4308a24</code> 
+     *
+     * @param id the download id in the database.
+     *
+     * @param value the status value i.e. ONLINE, ARCHIVE or RESTORING.
      */
     @PUT
     @Path("/download/{id}/status")
@@ -131,6 +142,17 @@ public class AdminResource {
         return Response.ok().build();
     }
     
+    /**
+     * Sets whether or not a download is deleted.
+     *
+     * @param icatUrl a url to a valid ICAT REST api.
+     * 
+     * @param sessionId a valid session id which takes the form <code>0d9a3706-80d4-4d29-9ff3-4d65d4308a24</code> 
+     *
+     * @param id the download id in the database.
+     *
+     * @param value either true or false.
+     */
     @PUT
     @Path("/download/{id}/isDeleted")
     @Produces({MediaType.APPLICATION_JSON})
