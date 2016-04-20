2aca875c661638641de607fd63c0be9e04d08766
Jody Salt <jody.salt@stfc.ac.uk>
2016-04-20 15:11:23 +0100

    doi redirect now works - close #212

--- yo/app/scripts/controllers/doi-redirect.controller.js
+++ yo/app/scripts/controllers/doi-redirect.controller.js
@@ -6,7 +6,19 @@
     var app = angular.module('angularApp');
 
     app.controller('DoiRedirectController', function($state, tc){
+    	var facilityName = $state.params.facilityName;
+    	var entityType = $state.params.entityType;
+    	var entityId = $state.params.entityId;
 
+    	tc.icat(facilityName).entity(entityType, ["where ?.id = ", entityType.safe(), entityId]).then(function(entity){
+    		if(entityType == 'datafile'){
+    			entity.parent().then(function(entity){
+    				entity.browse();
+    			});
+    		} else {
+    			entity.browse();
+    		}
+    	});
     });
 
 })();
