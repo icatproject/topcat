35944b834705dee75083be80a0d2cdb82fcaac9b
Jody Salt <jody.salt@stfc.ac.uk>
2016-04-21 14:56:39 +0100

    fix

--- yo/app/scripts/controllers/browse-entities.controller.js
+++ yo/app/scripts/controllers/browse-entities.controller.js
@@ -66,7 +66,7 @@
                 }
             });
 
-            if($state.params.instrumentId && (entityType == 'investigation' || $state.params.investigationId)){
+            if($state.params.instrumentId && (entityType == 'investigation' || $state.params.investigationId || $state.params.facilityCycleId)){
                 out.where("investigationInstrument.id = instrument.id")
             }
 
