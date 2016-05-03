0ce9172ec5fb2a6863bce2d2b9392e3f7f80fcbf
Jody Salt <jody.salt@stfc.ac.uk>
2016-05-03 13:23:29 +0100

    fix travis?

--- ci/travis.rb
+++ ci/travis.rb
@@ -114,7 +114,7 @@ exec %{
   sudo ./setup install
   cd ../
 
-  sudo /opt/glassfish4/bin/asadmin -t set applications.application.topcat-2.0.0-SNAPSHOT.deployment-order=140
+  sudo /opt/glassfish4/bin/asadmin -t set applications.application.topcat-2.0.1-SNAPSHOT.deployment-order=140
 
   mysql -u root --password=secret --host=127.0.0.1 icat < ./provision/icat.sql
   sudo apt-get --assume-yes install ruby-dev
