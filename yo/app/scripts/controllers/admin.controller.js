25e1aa555b9178f4cb850c780a93e445f3607584
Jody Salt <jody.salt@stfc.ac.uk>
2016-05-05 16:03:31 +0100

    sorting and filters in admin now works - close #244

--- yo/app/scripts/controllers/admin.controller.js
+++ yo/app/scripts/controllers/admin.controller.js
@@ -9,7 +9,6 @@
       var page = 1;
       var gridApi;
       var pageSize = 10;
-      var filters = ["1 = 1"];
       var timeout = $q.defer();
       $scope.$on('$destroy', function(){
           timeout.resolve();
@@ -38,6 +37,19 @@
           cellTemplate : '<div class="ui-grid-cell-contents"><button ng-click="grid.appScope.delete(row.entity)" ng-show="!row.entity.isDeleted">Delete</button><button ng-click="grid.appScope.restore(row.entity)" ng-show="row.entity.isDeleted">Restore</button> <button ng-click="grid.appScope.pause(row.entity)" ng-show="row.entity.status == \'RESTORING\'">Pause</button><button ng-click="grid.appScope.resume(row.entity)" ng-show="row.entity.status == \'PAUSED\'">Resume</button></div>'
       });
 
+      var sortColumns = [];
+      _.each(this.gridOptions.columnDefs, function(columnDef){
+        if(columnDef.sort){
+          sortColumns.push({
+            colDef: {field: columnDef.field},
+            sort: columnDef.sort
+          })
+        }
+      });
+      sortColumns = _.sortBy(sortColumns, function(sortColumn){
+        return sortColumn.sort.priority;
+      });
+
       function updateScroll(resultCount){
           $timeout(function(){
               var isMore = resultCount == pageSize;
@@ -47,14 +59,14 @@
       }
 
       function getPage(){
-        return admin.downloads(filters).then(function(downloads){
+        return admin.downloads(generateQuery()).then(function(downloads){
           _.each(downloads, function(download){ download.getSize(timeout.promise); });
           return downloads;
         });
       }
 
-      function updateFilterQuery(){
-          filters = ['1 = 1'];
+      function generateQuery(){
+          var out = ['1 = 1'];
           _.each(that.gridOptions.columnDefs, function(columnDef){
               if(columnDef.type == 'date' && columnDef.filters){
                   var from = columnDef.filters[0].term || '';
@@ -62,7 +74,7 @@
                   if(from != '' || to != ''){
                       from = helpers.completePartialFromDate(from);
                       to = helpers.completePartialToDate(to);
-                      filters.push([
+                      out.push([
                           "and download.? between {ts ?} and {ts ?}",
                           columnDef.field.safe(),
                           from,
@@ -70,13 +82,25 @@
                       ]);
                   }
               } else if(columnDef.type == 'string' && columnDef.filter && columnDef.filter.term) {
-                  filters.push([
+                  out.push([
                       "and UPPER(download.?) like concat('%', ?, '%')", 
                       columnDef.field.safe(),
                       columnDef.filter.term.toUpperCase()
                   ]);
               }
           });
+          
+          if(sortColumns.length > 0){
+            out.push("order by");
+            _.each(sortColumns, function(sortColumn, i){
+                if(sortColumn.colDef){
+                    out.push(['download.? ?', sortColumn.colDef.field.safe(), sortColumn.sort.direction.safe()]);
+                    if(i < sortColumns.length - 1) out.push(',');
+                }
+            });
+          }
+
+          return out;
       }
 
       this.pause = function(download){
@@ -113,9 +137,15 @@
 
             gridApi.core.on.filterChanged($scope, function() {
                 page = 1;
+                getPage().then(function(downloads){
+                    that.gridOptions.data = downloads;
+                    updateScroll(downloads.length);
+                });
+            });
 
-                updateFilterQuery();
-
+            gridApi.core.on.sortChanged($scope, function(grid, _sortColumns){
+                sortColumns = _sortColumns;
+                page = 1;
                 getPage().then(function(downloads){
                     that.gridOptions.data = downloads;
                     updateScroll(downloads.length);
