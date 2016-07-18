1b0c6e8798a61bbaf54e223917d37b60570db3ab
Jody Salt <jody.salt@stfc.ac.uk>
2016-07-18 13:38:24 +0100

    removed Singleton annotation

--- src/main/java/org/icatproject/topcat/repository/CartRepository.java
+++ src/main/java/org/icatproject/topcat/repository/CartRepository.java
@@ -23,7 +23,6 @@ import org.slf4j.LoggerFactory;
 
 @Stateless
 @LocalBean
-@Singleton
 public class CartRepository {
     private static final Logger logger = LoggerFactory.getLogger(CartRepository.class);
 
