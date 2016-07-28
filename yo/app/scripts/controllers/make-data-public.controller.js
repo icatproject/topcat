058fb688d922e88f43fc7cca877a56fda87892f4
Jody Salt <jody.salt@stfc.ac.uk>
2016-07-28 16:17:49 +0100

    partially implemented dialog sequence #211

--- yo/app/scripts/controllers/make-data-public.controller.js
+++ yo/app/scripts/controllers/make-data-public.controller.js
@@ -12,31 +12,49 @@
     	this.releaseDate = null;
     	this.isReleaseDateOpen = false;
     	this.licence = null;
-    	this.acceptLegal = false;
+    	this.hasAcceptLegal = false;
     	this.dateFormat = 'yyyy-MM-dd';
 
     	var states = ['release_date', 'legal', 'confirmation'];
 
-    	this.next = function(){
-    		if(this.state == 'release_date' && (!this.isReleaseDate || this.releaseDate != null)){
-    			this.isReleaseDateOpen = false;
-    			this.state = 'legal';
-	    	} else if(this.state == 'legal' && this.licence != null && this.acceptLegal){
-	    		this.state = 'confirm';
-	    	}
+    	this.isPreviousDisabled = function(){
+    		return this.state == 'release_date';
     	};
 
     	this.previous = function(){
     		if(this.state == 'confirm'){
     			this.state = 'legal';
-    			this.acceptLegal = false;
+    			this.hasAcceptLegal = false;
     		} else if(this.state == 'legal'){
     			this.state = 'release_date';
     		}
     	};
 
+    	this.isNextDisabled = function(){
+    		if(this.state == 'release_date' && (!this.isReleaseDate || this.releaseDate != null)){
+    			return false;
+    		}
+
+    		if(this.state == 'legal' && this.acceptedLegal){
+    			return false;
+    		}
+
+    		return true;
+    	};
+
+    	this.next = function(){
+    		this.nextEnabled = false;
+
+    		if(this.state == 'release_date' && (!this.isReleaseDate || this.releaseDate != null)){
+    			this.isReleaseDateOpen = false;
+    			this.state = 'legal';
+	    	} else if(this.state == 'legal' && this.licence != null && this.hasAcceptLegal){
+	    		this.state = 'confirm';
+	    	}
+    	};
+
     	this.confirm = function(){
-    		
+
     	};
 
     	this.cancel = function() {
