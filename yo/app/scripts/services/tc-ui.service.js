43ed84d255fa6407342b1d0d7346da1ecba55793
Jody Salt <jody.salt@stfc.ac.uk>
2016-08-03 10:02:58 +0100

    added to plugin mechanism #265

--- yo/app/scripts/services/tc-ui.service.js
+++ yo/app/scripts/services/tc-ui.service.js
@@ -5,7 +5,7 @@
 
     var app = angular.module('topcat');
 
-    app.service('tcUi', function(helpers){
+    app.service('tcUi', function($rootScope, helpers, RuntimeStatesProvider){
 
     	this.create = function(tc){
     		return new Ui(tc);
@@ -15,9 +15,30 @@
 
             var mainTabs = [];
 
+            this.mainTabs = function(){
+                return mainTabs;
+            };
+
             this.registerMainTab = helpers.overload({
                 'string, string, object': function(name, view, options){
-                    
+                    mainTabs.push({name: name, view: view, options: options});
+
+                    var state = {
+                      url: '/' + name,
+                      resolve: {
+                        authenticate: function(Authenticate) {
+                            return Authenticate.authenticate();
+                        }
+                      },
+                      views: {},
+                      controller: options.controller
+                    };
+
+                    state.views[name + '@home'] = { templateUrl: view };
+
+                    RuntimeStatesProvider.addState('home.' + name, state);
+
+                    $rootScope.$broadcast('tab:change');
                 },
                 'string, string': function(name, view){
                     this.registerMainTab(name, view, {});
