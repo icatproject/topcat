ae69e85324d44f9a84759ae88c2fd3e870c72626
Jody Salt <jody.salt@stfc.ac.uk>
2016-08-15 11:49:29 +0100

    added in verifyPassword method

--- yo/app/scripts/services/tc-icat.service.js
+++ yo/app/scripts/services/tc-icat.service.js
@@ -128,6 +128,23 @@
     			});
     		};
 
+            this.verifyPassword = function(password){
+                var params = {
+                    json: JSON.stringify({
+                        plugin: this.session().plugin,
+                        credentials: [
+                            {username: this.session().username.replace(/^[^\/]*\//, '')},
+                            {password: password}
+                        ]
+                    })
+                };
+                return this.post('session', params).then(function(response){
+                    return true;
+                }, function(){
+                    return false;
+                });
+            };
+
             this.logout = helpers.overload({
             	'boolean': function(isSoft){
             		var promises = [];
