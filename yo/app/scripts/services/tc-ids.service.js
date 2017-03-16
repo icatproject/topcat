4bae07647163fd532a95af3546179bac4ef1781b
Jody Salt <jody.salt@stfc.ac.uk>
2017-03-16 15:42:14 +0000

    improved documentation

--- yo/app/scripts/services/tc-ids.service.js
+++ yo/app/scripts/services/tc-ids.service.js
@@ -27,6 +27,14 @@
           return cache;
         };
 
+
+        /**
+         * Returns the version of the IDS server
+         *
+         * @method
+         * @name IDS#version
+         * @return {Promise<string>}
+         */
     		this.version = function(){
     			var out = $q.defer();
     			this.get('getApiVersion').then(function(version){
@@ -36,20 +44,55 @@
     		};
 
         this.isTwoLevel = helpers.overload({
+          /**
+           * Returns whether or not the IDS is two level.
+           *
+           * @method
+           * @name IDS#isTwoLevel
+           * @param {object} options {@link https://docs.angularjs.org/api/ng/service/$http#usage|as specified in the Angular documentation}
+           * @return {Promise<boolean>}
+           */
           'object': function(options){
             return this.get('isTwoLevel', {}, options).then(function(isTwoLevel){
               return isTwoLevel == 'true';
             });
           },
+
+          /**
+           * Returns whether or not the IDS is two level.
+           *
+           * @method
+           * @name IDS#isTwoLevel
+           * @param {Promise} timeout if resolved will cancel the request
+           * @return {Promise<boolean>}
+           */
           'promise': function(timeout){
             return this.isTwoLevel({timeout: timeout});
           },
+
+          /**
+           * Returns whether or not the IDS is two level.
+           *
+           * @method
+           * @name IDS#isTwoLevel
+           * @return {Promise<boolean>}
+           */
           '': function(){
             return this.isTwoLevel({});
           }
         });
 
         this.upload = helpers.overload({
+          /**
+           * Uploads files to a dataset.
+           *
+           * @method
+           * @name IDS#upload
+           * @param {number} datasetId the id of the dataset you wish to upload to 
+           * @param {File[]} files the files to be uploaded
+           * @param {object} options {@link https://docs.angularjs.org/api/ng/service/$http#usage|as specified in the Angular documentation}
+           * @return {Promise}
+           */
           'number, array, object': function(datasetId, files, options){
             var promises = [];
 
@@ -72,10 +115,31 @@
             
             return $q.all(promises);
           }
+
+          /**
+           * Uploads files to a dataset.
+           *
+           * @method
+           * @name IDS#upload
+           * @param {number} datasetId the id of the dataset you wish to upload to 
+           * @param {File[]} files the files to be uploaded
+           * @param {Promise} timeout if resolved will cancel the request
+           * @return {Promise}
+           */
           ,
           'promise, number, array': function(timeout, datasetId, files){
             return this.upload(datasetId, files, {timeout: timeout});
           },
+
+          /**
+           * Uploads files to a dataset.
+           *
+           * @method
+           * @name IDS#upload
+           * @param {number} datasetId the id of the dataset you wish to upload to 
+           * @param {File[]} files the files to be uploaded
+           * @return {Promise}
+           */
           'number, array': function(datasetId, files){
             return this.upload(datasetId, files, {});
           }
