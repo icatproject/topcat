60adf7ee195d6cbdd09b5b371fc4f5633334db1b
Jody Salt <jody.salt@stfc.ac.uk>
2017-06-22 13:17:32 +0100

    simplified icat entity service

--- yo/app/scripts/services/tc-icat-entity.service.js
+++ yo/app/scripts/services/tc-icat-entity.service.js
@@ -242,198 +242,49 @@
 				});
 			}
 
-			var parentFunctions = {
-				datafile: function(datafile, options){
-					var defered = $q.defer();
-					var dataset = datafile.dataset;
-					if(dataset){
-						defered.resolve(_.merge(dataset, {entityType: 'dataset'}));
-					} else {
-						icat.query([
-							'select dataset from Dataset dataset',
-							', dataset.datafiles datafile',
-							'where datafile.id = ?', datafile.id
-						], options).then(function(datasets){
-							datafile.dataset = datasets[0];
-							defered.resolve(dataset[0]);
-						}, function(response){
-							defered.reject(response);
-						});
-					}
-					return defered.promise
-				},
-				dataset: function(dataset, options){
-					var defered = $q.defer();
-					var investigation = dataset.investigation;
-					if(investigation){
-						defered.resolve(_.merge(investigation, {entityType: 'investigation'}));
-					} else {
-						icat.query([
-							'select investigation from Investigation investigation',
-							', investigation.datasets dataset',
-							'where dataset.id = ?', dataset.id
-						], options).then(function(investigations){
-							dataset.investigation = investigations[0];
-							defered.resolve(investigations[0]);
-						}, function(response){
-							defered.reject(response);
-						});
-					}
-					return defered.promise
-				},
-				investigation: function(investigation, options){
-					var defered = $q.defer();
-					var facilityCycle = investigation.facilityCycle;
-					if(facilityCycle){
-						defered.resolve(facilityCycle);
-					} else {
-						icat.query([
-							'select facilityCycle from FacilityCycle facilityCycle',
-							', facilityCycle.facility facility,',
-							'facility.investigations investigation',
-							'where facility.id = ?', facility.config().id,
-							'and investigation.id = ?', investigation.id,
-							'and investigation.startDate BETWEEN facilityCycle.startDate AND facilityCycle.endDate'
-						], options).then(function(facilityCycles){
-							investigation.facilityCycle = facilityCycles[0];
-							defered.resolve(facilityCycles[0]);
-						}, function(response){
-							defered.reject(response);
-						});
-					}
-					return defered.promise;
-				},
-				facilityCycle: function(facilityCycle, childEntity, options){
-					if(!_.includes(['investigation', 'dataset', 'datafile'], childEntity.entityType)){
-						return $q.resolve(null);
-					}
-					return childEntity.thisOrParent('investigation').then(function(investigation){
-						var defered = $q.defer();
-						var instrument = facilityCycle.instrument;
-						if(instrument){
-							defered.resolve(instrument);
-						} else {
-							icat.query([
-								'select instrument from Instrument instrument',
-								', instrument.investigationInstruments investigationInstrument,',
-								'investigationInstrument.investigation investigation,',
-								'instrument.facility facility',
-								'where facility.id = ?', facility.config().id,
-								'and investigation.id = ?', investigation.id,
-							], options).then(function(instruments){
-								facilityCycle.instrument = instruments[0];
-								defered.resolve(instruments[0]);
-							}, function(response){
-								defered.reject(response);
-							});
-						}
-						return defered.promise;
+			this.parent = function(){
+				var hierarchy = facility.config().hierarchy;
+				var positionInHierarchy = _.indexOf(hierarchy, this.entityType);
+				var parentEntityType = hierarchy[positionInHierarchy - 1];
+				if(parentEntityType != 'facility'){
+					var queryBuilder = icat.queryBuilder(parentEntityType);
+					queryBuilder.where(["?.id = ?", this.entityType.safe(), this.id]);
+					return queryBuilder.run().then(function(entities){
+						return entities[0];
 					});
-				}
-	
-			};
-
-
-			var parent; 
-			this.parent = helpers.overload({
-				'string, object, object': function(entityType, childEntity, options){
-					return this.parent(childEntity, options).then(function(entity){
-						if(!entity || entity.entityType == entityType){
-							return entity;
-						} else {
-							return entity.parent(entityType, childEntity, options);
-						}
-					});
-				},
-				'string, object, promise': function(entityType, childEntity, timeout){
-					return this.parent(entityType, childEntity, {timeout: timeout});	
-				},
-				'string, object': function(entityType, childEntity){
-					return this.parent(entityType, childEntity, {});
-				},
-				'object, object': function(childEntity, options){
-					var defered = $q.defer();
-					if(parent !== undefined){
-						defered.resolve(parent);
-					} else {
-						var parentFunction = parentFunctions[this.entityType];
-						if(parentFunction){
-							parentFunction(this, childEntity, options).then(function(_parent){
-								parent = _parent;
-								defered.resolve(parent);
-							});
-						} else {
-							parent = null;
-							defered.resolve(parent);
-						}
-					}
-					return defered.promise;
-				},
-				'object': function(childEntity){
-					return this.parent(childEntity, {});	
-				},
-				'object, promise': function(childEntity, timeout){
-					return this.parent(childEntity, {timeout: timeout});	
-				},
-				'promise': function(timeout){
-					return this.parent(entity, {timeout: timeout});	
-				},
-				'': function(){
-					return this.parent(entity, {});	
-				}
-			});
-
-			this.thisOrParent = function(entityType){
-				if(this.entityType == entityType){
-					return $q.resolve(this);
 				} else {
-					return this.parent(entityType, this);
+					return $q.resolve(null);
 				}
 			};
-				
 
 			this.ancestors = function(){
 				var defered = $q.defer();
 				var out = [];
-				var childEntity = this;
-				function parent(){
-					this.parent(childEntity).then(function(entity){
+
+				function parent(entity){
+					entity.parent().then(function(entity){
 						if(entity){
 							out.push(entity);
-							parent.call(entity);
+							parent(entity);
 						} else {
 							defered.resolve(out);
 						}
 					});
 				}
+				parent(this);
 
-				parent.call(this);
 				return defered.promise;
 			};
 
-			this.thisAndAncestors = function(){
+			this.stateParams = function(){
 				var that = this;
 				return this.ancestors().then(function(ancestors){
-					return _.flatten([that, ancestors]);
-				});
-			};
-
-			this.stateParams = function(){
-				if($state.current.name.match(/^home\.browse\.facility\./)){
-					var out = _.clone($state.params);
-					delete out.uiGridState;
-					out[helpers.uncapitalize(this.entityType) + "Id"] = this.id;
-					return $q.resolve(out);
-				} else {
-					return this.thisAndAncestors().then(function(thisAndAncestors){
-						var out = {};
-						_.each(thisAndAncestors, function(entity){
-							out[entity.entityType + "Id"] = entity.id;
-							if(entity.entityType == 'investigation') out['proposalId'] = entity.name;
-						});
-						return _.merge(out, {facilityName: facilityName});
+					var out = [];
+					_.each(ancestors.concat([that]), function(entity){
+						out[entity.entityType + "Id"] = entity.id;
 					});
-				}
+					return _.merge(out, {facilityName: facilityName});
+				});
 			};
 
 			/**
