11307c7e1fc1e6a175417cd372e936d27021aa2b
Jody Salt <jody.salt@stfc.ac.uk>
2017-08-14 14:24:26 +0100

    fix travis?

--- yo/test/protractor.conf.js
+++ yo/test/protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // Spec patterns are relative to the location of this config.
   specs: [
