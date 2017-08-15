88a78ba013469edf29e004da4032746403d3ac68
Jody Salt <jody.salt@stfc.ac.uk>
2017-08-15 10:49:58 +0100

    updated test

--- yo/test/spec/e2e/user.js
+++ yo/test/spec/e2e/user.js
@@ -2,9 +2,9 @@
 
 describe('user', function() {
   it('creates download and then deletes it', function() {
-    // browser.get('/');
+    browser.get('/');
 
-    // browser.waitForAngular();
+    browser.waitForAngular();
 
     // element(by.model('loginController.userName')).sendKeys('root');
     // element(by.model('loginController.password')).sendKeys('root');
