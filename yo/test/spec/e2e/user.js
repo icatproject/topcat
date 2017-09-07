b71efad29eee37edb940393ead944bd48b3734a4
Jody Salt <jody.salt@stfc.ac.uk>
2017-09-07 13:49:56 +0100

    fix travis?

--- yo/test/spec/e2e/user.js
+++ yo/test/spec/e2e/user.js
@@ -3,62 +3,61 @@
 describe('user', function() {
   it('creates download and then deletes it', function() {
 
-    browser.ignoreSynchronization = true;
+    //browser.ignoreSynchronization = true;
 
     browser.get('http://localhost:8080/#/login');
 
-    browser.driver.wait(element(by.css('.is-init')).isPresent()).then(function(){
+    var until = protractor.ExpectedConditions;
+    browser.wait(until.presenceOf(element(by.css('.is-init')))).then(function(){
         browser.ignoreSynchronization = false;
     });
 
-    browser.waitForAngular();
-    
     element(by.model('loginController.userName')).sendKeys('root');
     element(by.model('loginController.password')).sendKeys('root');
     element(by.id('login')).click();
 
     browser.waitForAngular();
 
-    // expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/my-data/LILS');
-    // expect(element(by.className('glyphicon-download-alt')).isPresent()).toEqual(false);
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/my-data/LILS');
+    expect(element(by.className('glyphicon-download-alt')).isPresent()).toEqual(false);
 
-    // element(by.css('a[ng-click="grid.appScope.browse(row.entity)"]')).click();
+    element(by.css('a[ng-click="grid.appScope.browse(row.entity)"]')).click();
 
-    // browser.waitForAngular();
+    browser.waitForAngular();
 
-    // element(by.css('a[ng-click="grid.appScope.browse(row.entity)"]')).click();
+    element(by.css('a[ng-click="grid.appScope.browse(row.entity)"]')).click();
 
-    // element(by.className('ui-grid-icon-ok')).click();
+    element(by.className('ui-grid-icon-ok')).click();
 
-    // browser.waitForAngular();
+    browser.waitForAngular();
 
-    // browser.wait(function(){
-    //     return element(by.className('glyphicon-shopping-cart')).isPresent();
-    // }, 1000 * 10, ".glyphicon-shopping-cart not present");
+    browser.wait(function(){
+        return element(by.className('glyphicon-shopping-cart')).isPresent();
+    }, 1000 * 10, ".glyphicon-shopping-cart not present");
 
-    // element(by.className('glyphicon-shopping-cart')).click();
+    element(by.className('glyphicon-shopping-cart')).click();
 
-    // browser.waitForAngular();
+    browser.waitForAngular();
 
-    // element(by.css('button[translate="CART.DOWNLOAD_CART_BUTTON.TEXT"]')).click();
+    element(by.css('button[translate="CART.DOWNLOAD_CART_BUTTON.TEXT"]')).click();
 
-    // browser.waitForAngular();
+    browser.waitForAngular();
 
-    // element(by.css('button[translate="CART.DOWNLOAD.MODAL.BUTTON.OK.TEXT"]')).click();
+    element(by.css('button[translate="CART.DOWNLOAD.MODAL.BUTTON.OK.TEXT"]')).click();
 
-    // browser.wait(function(){
-    //     return element(by.className('glyphicon-download-alt')).isPresent();
-    // }, 1000 * 10, ".glyphicon-download-alt not present");
+    browser.wait(function(){
+        return element(by.className('glyphicon-download-alt')).isPresent();
+    }, 1000 * 10, ".glyphicon-download-alt not present");
 
-    // element(by.className('glyphicon-download-alt')).click();
+    element(by.className('glyphicon-download-alt')).click();
 
-    // browser.wait(function(){
-    //     return element(by.css('a[translate="DOWNLOAD.ACTIONS.LINK.REMOVE.TEXT"]')).isPresent();
-    // });
+    browser.wait(function(){
+        return element(by.css('a[translate="DOWNLOAD.ACTIONS.LINK.REMOVE.TEXT"]')).isPresent();
+    });
 
-    // element(by.css('a[translate="DOWNLOAD.ACTIONS.LINK.REMOVE.TEXT"]')).click();
+    element(by.css('a[translate="DOWNLOAD.ACTIONS.LINK.REMOVE.TEXT"]')).click();
 
-    // expect(element(by.className('glyphicon-download-alt')).isPresent()).toEqual(false);
+    expect(element(by.className('glyphicon-download-alt')).isPresent()).toEqual(false);
   });
 
 
