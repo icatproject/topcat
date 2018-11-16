1222095ba8fd3760865adf153ce412d982a53adb
Brian Ritchie <brian.ritchie@stfc.ac.uk>
2018-11-16 10:44:02 +0000

    DOI redirection: inform users when entity query fails
    If the DOI entity query returns no results, or a null result, assume this is because the user cannot read the entity, and inform them. (Previously, this would result in a TypeError message.)
    We could go to the default home page, but a "blank" page makes it more obvious that the request cannot be satisfied; the user could always click the Home button if they want to continue.
    
    Fixes #388 .

--- yo/app/scripts/controllers/doi-redirect.controller.js
+++ yo/app/scripts/controllers/doi-redirect.controller.js
@@ -5,13 +5,24 @@
 
     var app = angular.module('topcat');
 
-    app.controller('DoiRedirectController', function($state, tc, helpers){
+    app.controller('DoiRedirectController', function($state, inform, tc, helpers){
     	var facilityName = $state.params.facilityName;
     	var entityType = $state.params.entityType;
     	var entityId = $state.params.entityId;
 
     	tc.icat(facilityName).query(["select ? from ? ? where ?.id = ", entityType.safe(), helpers.capitalize(entityType).safe(), entityType.safe(),entityType.safe(), entityId]).then(function(entities){
-    		if(entityType == 'datafile'){
+    		if( ! entities || ! entities[0] ){
+    			// Query may return [] or null if the entity ID is unknown, [null] if user has no read access
+    			// In these cases, inform the user.
+    			// We could go to the default home page in this case, but BR prefers to leave the page blank.
+    			// If we *do* set a particular state, it has to happen before the inform.
+    			// var state = tc.config().home == 'browse' ? 'home.browse.facility' : 'home.' + tc.config().home;
+    	        // $state.go(state);
+    			inform.add("Cannot read the " + entityType + ". You may not have read access, or it may not be published yet", {
+    				'ttl' : -1,
+    				'type' : 'warning'
+    			});
+    		} else if(entityType == 'datafile'){
     			entities[0].parent().then(function(entity){
     				entity.browse();
     			});
