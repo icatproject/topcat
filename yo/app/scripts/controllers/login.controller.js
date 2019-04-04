8e1ff955dcaae7750cdb91449e110977ef47959c
Brian Ritchie <brian.ritchie@stfc.ac.uk>
2019-04-04 10:59:18 +0100

    Allow authenticators to have their own login buttons
    Added a showAsButton boolean property to authenticationTypes in topcat.json; when true, the authenticator is removed from the choice llist and replaced by its own button.
    
    If any button-showing authenticator requires username/password, the input boxes are always shown (I trust the reason is obvious).
    
    Known problem: the extra buttons are not disabled when the input boxes are empty. Clicking on a button that requires inputs results in an obscure/internal "could not satisfy overloaded method" error.
    
    There are some subtle points that can arise when there is a mix of button and non-button authenticators:
    
      - if a button authenticator requires username/password, then any 'anon' or external authenticator **must** also have its own button. This should be a configuration error, reported during startup, but is not (yet).
      - if login via a button authenticator fails, the selected authenticator will be reset to one in the choice list (if this is not empty). Without this, a choice list of one authenticator could become unreachable.

--- yo/app/scripts/controllers/login.controller.js
+++ yo/app/scripts/controllers/login.controller.js
@@ -12,6 +12,7 @@
         this.nonUserFacilities = tc.nonUserFacilities();
         if(this.nonUserFacilities[0]) this.facilityName = this.nonUserFacilities[0].config().name;
         this.authenticationTypes = [];
+        this.buttonAuthTypes = [];
         this.userName = "";
         this.password = "";
 
@@ -69,13 +70,48 @@
                 });
             }
         };
+        
+        this.buttonLogin = function(authenticationType){
+        	this.authenticationType = authenticationType;
+        	this.login();
+        	// At this point, the authenticationType won't be in the non-button authenticationTypes;
+        	// so if login fails, the chosen authenticator in the choice list will be blank;
+        	// and if the choice list contains just one authenticator, the user won't be able to change it!
+        	// So at this point, always reset the authenticationType to a non-button authenticator, if there is one
+        	if( this.authenticationTypes.length >= 1 ){
+        		this.authenticationType = this.authenticationTypes[0];
+        	}
+        };
+        
+        this.showCredInputs = function(){
+        	return true;
+        	// true if the currently-selected auth type requires credentials
+        	// OR if any of the button-based authenticators do
+        	if( this.authenticationType.plugin != 'anon' && !this.authenticationType.external ){
+        		return true;
+        	} else {
+        		return _.some(this.buttonAuthTypes,function(authenticationType){return authenticationType.plugin != 'anon' && !authenticationType.external;});
+        	};
+        };
 
         this.facilityChanged = function(){
             facility = tc.facility(this.facilityName);
-            this.authenticationType = facility.config().authenticationTypes[0];
             this.authenticationTypes = facility.config().authenticationTypes;
+            
+            // Split authenticationTypes into those that have / don't have buttons
+            this.buttonAuthTypes = _.select(this.authenticationTypes, function(authenticationType){return authenticationType.showAsButton;});
+            this.authenticationTypes = _.select(this.authenticationTypes, function(authenticationType){return !authenticationType.showAsButton;});
+            
+            // If there are still non-button auth types, choose the first initially
+            if( this.authenticationTypes.length > 0 ){
+            	this.authenticationType = this.authenticationTypes[0];
+            } else {
+            	// Unlikely to be used: only when there's only one external authType, and yet showAsButton is true
+            	this.authenticationType = this.buttonAuthTypes[0];
+            }
 
-            if(this.facilities.length <= 1 && this.authenticationTypes.length == 1 && this.authenticationType.external){
+            // If there's only one authenticator and it's external, then login directly
+            if(this.facilities.length <= 1 && this.authenticationTypes.length + this.buttonAuthTypes.length == 1 && this.authenticationType.external){
                 this.login();
             }
         };
