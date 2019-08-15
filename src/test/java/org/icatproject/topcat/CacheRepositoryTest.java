9d7ae678269f90c06d044ae2b5788fc7947b8089
Brian Ritchie <brian.ritchie@stfc.ac.uk>
2019-08-15 11:30:52 +0100

    Attempt to rewrite CacheRepositoryTest
    Following https://stackoverflow.com/a/20635285, tried to test CacheRepository using an embedded container. However, it does not appear to work. It may be that CacheRepository has to be declared explicitly using @Entity, and there may be other configuration changes required (e.g. to persistence.xml). All in all, this is turning out not to be the quick fix that I had hoped it would be, so I am using a branch to "archive" the attempt. It is unlikely that I will return to it!

--- src/test/java/org/icatproject/topcat/CacheRepositoryTest.java
+++ src/test/java/org/icatproject/topcat/CacheRepositoryTest.java
@@ -8,34 +8,47 @@ import org.junit.*;
 
 
 import javax.ejb.EJB;
-
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
 
 import org.icatproject.topcat.repository.CacheRepository;
 
 public class CacheRepositoryTest {
 
-	@EJB
-	private CacheRepository cacheRepository;
+    private static Context  ctx;
+    private static EJBContainer ejbContainer;
 
-	private static String sessionId;
+   @BeforeClass
+   public  static void setUp() {
+       ejbContainer = EJBContainer.createEJBContainer();
+       System.out.println("Opening the container" );
+       ctx = ejbContainer.getContext();
+   }
 
+   @AfterClass
+   public  static void tearDown() {
+       ejbContainer.close();
+       System.out.println("Closing the container" );
+   }
 
 	@Test
 	public void testPutAndGet() throws Exception {
-		//cacheRepository.put("test:1", "Hello World!");
-		//assertEquals( "Hello World!", (String) cacheRepository.get("test:1"));
+        CacheRepository cacheRepository = (CacheRepository) ctx.lookup("java:global/classes/CacheRepository");
+        assertNotNull(cacheRepository);
+		cacheRepository.put("test:1", "Hello World!");
+		assertEquals( "Hello World!", (String) cacheRepository.get("test:1"));
 	}
 
 	@Test
-	public void testRemove() {
+	public void testRemove() throws Exception {
 		// BR: this test requires the cacheRepository bean to be created,
 		// and I don't know how to do that. I don't think Jody did either,
 		// as all other tests that use cacheRepository have been commented-out!
-		/*
+        CacheRepository cacheRepository = (CacheRepository) ctx.lookup("java:global/classes/CacheRepository");
+        assertNotNull(cacheRepository);
 		String key = "test:remove";
 		cacheRepository.put(key, "Hello World");
 		cacheRepository.remove(key);
 		assertEquals(null,cacheRepository.get(key));
-		*/
 	}
 }
