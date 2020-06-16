73a124472e0d3d216036507337337ea5c5a562a9
Louise Davies <louise.davies@stfc.ac.uk>
2020-06-16 08:58:36 +0100

    remove python-software-properties from apt-get

--- ci/travis.rb
+++ ci/travis.rb
@@ -23,7 +23,7 @@ exec %{
 
   cd install
 
-  sudo apt-get --assume-yes install apache2 git software-properties-common python-software-properties unzip build-essential dos2unix
+  sudo apt-get --assume-yes install apache2 git software-properties-common unzip build-essential dos2unix
   sudo apt-get install libgconf2-dev -y
 
   echo "USE mysql; UPDATE user SET password=PASSWORD('secret') WHERE user='root'; FLUSH PRIVILEGES; " | mysql -u root
